f4368766d60a99cc192a57c179448b4911e1e46b
ANONIM <22611024@students.uii.ac.id>
2024-07-26 08:42:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import pickle
 import streamlit as st
-import pickle as pickle
 import numpy as np
 
 # Membaca model
-customer_model = pickle.load(open('model.sav','rb'))
+customer_model = pickle.load(open('model.sav', 'rb'))
 
 # Judul web
 st.title('Prediksi Harga Kendaraan')
@@ -22,7 +21,13 @@ Prediksi_Harga_Kendaraan = ''
 if st.button('Prediksi'):
     try:
         # Konversi input menjadi numerik
-        inputs = np.array([[float(year), float(mileage), float(tax), float(mpg),float(engineSize))]])
+        inputs = np.array([[float(year), float(mileage), float(tax), float(mpg), float(engineSize)]])
+        
         # Lakukan prediksi
         status_prediksi = customer_model.predict(inputs)
         
+        st.success(f'Harga prediksi kendaraan adalah: {status_prediksi[0]}')
+    except ValueError:
+        st.error("Pastikan semua input diisi dengan angka yang valid.")
+    except Exception as e:
+        st.error(f"Terjadi kesalahan: {e}")
